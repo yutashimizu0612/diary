402cad2bc416d0f32439b11a88f3fb017ccc1ad5
yutashimizu0612 <s08388yskh@gmail.com>
2021-02-11 09:00:10 +0900

    投稿コンポーネントの作成

---
+++ client/src/views/components/Post.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+const StyledPost = styled.div`
+  background: #fff;
+  border: 1px solid #d0caca;
+  border-radius: 8px;
+  font-weight: 300;
+  padding: 20px 30px 16px;
+`;
+
+const StyledUser = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledImage = styled.img`
+  border-radius: 50%;
+  width: 56px;
+  height: 56px;
+`;
+
+const StyledName = styled.span`
+  display: block;
+  font-weight: normal;
+`;
+
+const StyledDate = styled.span`
+  color: #707070;
+  font-size: 15px;
+`;
+
+const StyledList = styled.ul`
+  margin: 20px 0 0 24px;
+`;
+
+const StyledItem = styled.li`
+  font-size: 15px;
+  margin-left: 1em;
+  text-indent: -1em;
+  &:not(:first-child) {
+    margin-top: 5px;
+  }
+`;
+
+// type Props = {};
+
+const Button: React.FC = () => (
+  <StyledPost>
+    <div>
+      <StyledUser>
+        <StyledImage src="https://placehold.jp/150x150.png" />
+        <div
+          css={`
+            margin-left: 30px;
+          `}>
+          <StyledName>ユーザ名</StyledName>
+          <StyledDate>2020/12/31</StyledDate>
+        </div>
+      </StyledUser>
+    </div>
+    <StyledList>
+      <StyledItem>・5km走れた</StyledItem>
+      <StyledItem>
+        ・ポートフォリオ作成のためのワイヤー作業に着手できたポートフォリオ作成のためのワイヤー作業に着手できた
+      </StyledItem>
+      <StyledItem>・朝散歩に行けた</StyledItem>
+    </StyledList>
+  </StyledPost>
+);
+
+export default Button;
--- client/src/views/pages/Posts.tsx
+++ client/src/views/pages/Posts.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
+import Post from '../components/Post';
 
-const Button = styled.button`
-  background: #1877f2;
-  border: none;
-  box-shadow: 0 3px 4px rgb(0, 0, 0, 0.16);
-  color: #fff;
-  cursor: pointer;
-  font-size: 18px;
-  font-weight: bold;
-  outline: none;
-  padding: 16px 0;
-  text-align: center;
-  transition: 0.2s;
-  width: 100%;
-  &:hover {
-    opacity: 0.8;
-  }
+const StyledWrapper = styled.div`
+  margin: 130px auto 0;
+  width: 680px;
 `;
 
-const Posts: React.FC = () => <p>みんなの投稿ページ</p>;
+const Posts: React.FC = () => (
+  <StyledWrapper>
+    <ul>
+      <li
+        css={`
+          margin-bottom: 20px;
+        `}>
+        <Post />
+      </li>
+    </ul>
+  </StyledWrapper>
+);
 
 export default Posts;
