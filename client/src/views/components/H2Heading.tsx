c07a5febcf4a6c5b268671084a2e2c52632b3c14
yutashimizu0612 <s08388yskh@gmail.com>
2021-02-23 09:23:37 +0900

    styled-components/macroに変更

--- client/src/views/components/H2Heading.tsx
+++ client/src/views/components/H2Heading.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 
 const StyledHeading = styled.h2<{ color: string }>`
   background: ${(props) => props.color};
