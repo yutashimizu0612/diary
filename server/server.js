407eb090bb1607a2698d00bfacd6c7517c8186e5
yutashimizu0612 <s08388yskh@gmail.com>
2021-02-27 10:51:20 +0900

    morganインストール / package.jsonのrepository追加

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+// routes
 const authRouter = require('./routes/auth');
 
 const app = express();
-
 require('dotenv').config();
 
+// middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(morgan('dev'));
 app.use('/api', authRouter);
 
 app.get('/', (req, res) => {
