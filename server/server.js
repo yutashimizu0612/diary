89c80d76dbd36584fc0f44ae3ab13ecfd34c81fb
yutashimizu0612 <s08388yskh@gmail.com>
2021-02-27 14:09:44 +0900

    corsのインストールと設定

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 // routes
 const authRouter = require('./routes/auth');
 
@@ -10,13 +11,16 @@ require('dotenv').config();
 // middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
 app.use('/api', authRouter);
 
 app.get('/', (req, res) => {
   res.send('api is started.');
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is started on port ${port}.`);
 });
