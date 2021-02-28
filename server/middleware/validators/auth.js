d570cc1d59f8bcbf61dffe8bf521981e0af98fd1
yutashimizu0612 <s08388yskh@gmail.com>
2021-02-28 11:42:07 +0900

    ユーザ登録フォームのバリデーション追加

---
+++ server/middleware/validators/auth.js
@@ -0,0 +1,27 @@
+const { body } = require('express-validator');
+
+exports.signupFormvalidator = [
+  body('name').not().isEmpty().withMessage('名前は入力必須です'),
+  body('email')
+    .not()
+    .isEmpty()
+    .withMessage('メールアドレスは入力必須です')
+    .isEmail()
+    .withMessage('正しいメールアドレスを入力してください'),
+  body('password')
+    .not()
+    .isEmpty()
+    .withMessage('パスワードは入力必須です')
+    .isLength({ min: 7 })
+    .withMessage('7文字以上のパスワードを入力してください'),
+  body('confirm_password')
+    .not()
+    .isEmpty()
+    .withMessage('確認用パスワードは入力必須です')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('パスワードと確認用のパスワードが一致しません');
+      }
+      return true;
+    }),
+];
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,8 +2,11 @@ const express = require('express');
 const authRouter = express.Router();
 // controller
 const { signup, login, logout } = require('../controllers/authController');
+// validator
+const { signupFormValidator } = require('../middleware/validators/auth');
+const { executeValidation } = require('../middleware/validators');
 
-authRouter.get('/signup', signup);
+authRouter.post('/signup', signupFormValidator, executeValidation, signup);
 authRouter.get('/login', login);
 authRouter.get('/logout', logout);
 
