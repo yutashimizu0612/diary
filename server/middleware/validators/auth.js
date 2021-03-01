c1bad9849d61a27bd0957422116e3fa2ab68be11
yutashimizu0612 <s08388yskh@gmail.com>
2021-03-01 19:45:13 +0900

    変数名修正 / exportの記述方法を変更

--- server/middleware/validators/auth.js
+++ server/middleware/validators/auth.js
@@ -1,6 +1,6 @@
 const { body } = require('express-validator');
 
-exports.signupFormvalidator = [
+const signupFormValidator = [
   body('name').not().isEmpty().withMessage('名前は入力必須です'),
   body('email')
     .not()
@@ -25,3 +25,7 @@ exports.signupFormvalidator = [
       return true;
     }),
 ];
+
+module.exports = {
+  signupFormValidator,
+};
