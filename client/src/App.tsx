826f5ec3544fa0137e65fc067fb29e9d7f41d477
yutashimizu0612 <s08388yskh@gmail.com>
2021-03-19 16:32:44 +0900

    Routesコンポーネント作成 / App.tsxとindex.tsxの整理

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,13 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Login from './views/pages/Login';
-import Singup from './views/pages/Singup';
-import Posts from './views/pages/Posts';
-import Status from './views/pages/Status';
-import Day from './views/pages/Day';
-import AccountSettings from './views/pages/AccountSettings';
-import NotFound from './views/pages/NotFound';
+import GlobalStyle from './theme/globalStyles';
+import type {} from 'styled-components/cssprop';
+import Routes from './Routes';
 
 const App: React.FC = () => (
-  <Switch>
-    <Route exact path="/">
-      <Day />
-    </Route>
-    <Route path="/login">
-      <Login />
-    </Route>
-    <Route path="/signup">
-      <Singup />
-    </Route>
-    <Route path="/posts">
-      <Posts />
-    </Route>
-    <Route path="/status">
-      <Status />
-    </Route>
-    <Route path="/settings">
-      <AccountSettings />
-    </Route>
-    <Route>
-      <NotFound />
-    </Route>
-  </Switch>
+  <>
+    <GlobalStyle />
+    <Routes />
+  </>
 );
 
 export default App;
