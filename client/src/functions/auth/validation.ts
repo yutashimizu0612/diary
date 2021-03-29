e90639d4638bfca88715994c94d81105abbc5c43
yutashimizu0612 <s08388yskh@gmail.com>
2021-03-29 20:35:45 +0900

    ユーザ登録時のバリデーションのバグ修正（未入力時に想定の文言が表示されない）

--- client/src/functions/auth/validation.ts
+++ client/src/functions/auth/validation.ts
@@ -9,23 +9,23 @@ export function validateSignUpForm(values: SignUpFormValues) {
   // メールアドレス
   if (!values.email) {
     Object.assign(errors, { email: 'メールアドレスは入力必須です。' });
-  }
-
-  const emailRegexp = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/;
-  if (!emailRegexp.test(values.email)) {
-    Object.assign(errors, { email: '正しいメールアドレスを入力してください' });
+  } else {
+    const emailRegexp = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/;
+    if (!emailRegexp.test(values.email)) {
+      Object.assign(errors, { email: '正しいメールアドレスを入力してください' });
+    }
   }
 
   // パスワード
   if (!values.password) {
     Object.assign(errors, { password: 'パスワードは入力必須です。' });
-  }
-
-  const passwordRegexp = /^(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,15}$/i;
-  if (!passwordRegexp.test(values.password)) {
-    Object.assign(errors, {
-      password: '半角英数字を用いて、7文字以上15文字以内で入力してください。',
-    });
+  } else {
+    const passwordRegexp = /^(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,15}$/i;
+    if (!passwordRegexp.test(values.password)) {
+      Object.assign(errors, {
+        password: '半角英数字を用いて、7文字以上15文字以内で入力してください。',
+      });
+    }
   }
 
   // 確認用パスワード
