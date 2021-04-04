0e0cdccd79b8afc5058fdd009cb135270c7f93ac
yutashimizu0612 <s08388yskh@gmail.com>
2021-04-05 08:09:18 +0900

    プロフィール表示部分を別コンポーネントに切り出し / APIからユーザ情報を取得

--- client/src/views/pages/AccountSettings.tsx
+++ client/src/views/pages/AccountSettings.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/macro';
+import axios from 'axios';
 import Layout from '../layouts/Layout';
 import UpdateAccountForm from '../components/UpdateAccountForm';
+import Profile from '../components/Profile';
+import { useAuth } from '../../hooks/use-auth';
 
 const StyledWrapper = styled.div`
   background: #fff;
@@ -19,72 +22,51 @@ const StyledTitle = styled.h2`
   text-align: center;
 `;
 
-const StyledTerm = styled.dt`
-  font-weight: bold;
-  margin-bottom: 12px;
-`;
-
-const StyledData = styled.dd`
-  margin-bottom: 50px;
-`;
-
-const StyledImage = styled.img`
-  border-radius: 50%;
-  width: 56px;
-  height: 56px;
-`;
-
-const StyledButton = styled.button`
-  background: #fff;
-  border: 1px solid #808080;
-  border-radius: 16px;
-  color: #808080;
-  padding: 2px 12px 0;
-  margin-left: 12px;
-  &:hover {
-    opacity: 0.7;
-  }
-`;
-
 type State = {
   name: string;
+  email: string;
 };
 
 const AccountSettings: React.FC = () => {
+  const auth = useAuth();
+  const token = auth.getAccessToken();
   const [values, setValues] = useState<State>({
     name: '',
+    email: '',
   });
   const [errors, setErrors] = useState({});
   const [isEditing, setIsEditing] = useState(false);
-
-  const edit = () => {
-    setIsEditing(true);
-  };
-
-  const Profile = () => (
-    <dl>
-      <StyledTerm>
-        プロフィール画像<StyledButton>変更する</StyledButton>
-      </StyledTerm>
-      <StyledData>
-        <StyledImage src="https://placehold.jp/150x150.png" />
-      </StyledData>
-      <StyledTerm>
-        ニックネーム<StyledButton onClick={() => edit()}>変更する</StyledButton>
-      </StyledTerm>
-      <StyledData>鈴木 尚典</StyledData>
-      <StyledTerm>メールアドレス</StyledTerm>
-      <StyledData>sample@gmail.com</StyledData>
-    </dl>
-  );
 
   const handleChange = (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
     setValues({ ...values, [prop]: event.target.value });
   };
 
+  const edit = () => {
+    setIsEditing(true);
+  };
+
   const quitEditing = () => {
     setIsEditing(false);
   };
+
+  const loadUserInfo = () => {
+    console.log('auth.user', auth.user);
+    axios({
+      method: 'GET',
+      url: `${process.env.REACT_APP_API_URL}/user/${auth.user.id}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((response) => {
+      console.log('LOAD USER INFO', response);
+      const { name, email } = response.data;
+      setValues({ ...values, name, email });
+    });
+  };
+
+  useEffect(() => {
+    loadUserInfo();
+  }, []);
 
   return (
     <Layout>
@@ -99,7 +81,7 @@ const AccountSettings: React.FC = () => {
             onBackButton={quitEditing}
           />
         ) : (
-          <Profile />
+          <Profile name={values.name} email={values.email} onClick={() => edit()} />
         )}
       </StyledWrapper>
     </Layout>
