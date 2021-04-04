51f2ed1d0014726fa6d9d528b3320911f0d471cb
yutashimizu0612 <s08388yskh@gmail.com>
2021-04-04 11:13:16 +0900

    PrivateルートやHeaderでのログイン判定をCustom hookを使用したものに変更

--- client/src/views/components/auth/PrivateRoute.tsx
+++ client/src/views/components/auth/PrivateRoute.tsx
@@ -1,25 +1,28 @@
 import React, { ReactNode } from 'react';
 import { Route, Redirect, RouteProps } from 'react-router-dom';
-import { isLoggedIn } from '../../../functions/auth/authenticate';
+import { useAuth } from '../../../hooks/use-auth';
 
 type Props = { children: ReactNode } & RouteProps;
 
-const PrivateRoute = ({ children, ...rest }: Props) => (
-  <Route
-    {...rest}
-    render={({ location }) =>
-      isLoggedIn() ? (
-        children
-      ) : (
-        <Redirect
-          to={{
-            pathname: '/login',
-            state: { from: location },
-          }}
-        />
-      )
-    }
-  />
-);
+const PrivateRoute = ({ children, ...rest }: Props) => {
+  const auth = useAuth();
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        auth.user ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
 
 export default PrivateRoute;
