4503a20047238522c6086736b066fd7d3005cdb2
yutashimizu0612 <s08388yskh@gmail.com>
2021-04-10 07:47:31 +0900

    styledコンポーネント作成

--- client/src/views/components/DiaryAccomplishment.tsx
+++ client/src/views/components/DiaryAccomplishment.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import styled from 'styled-components/macro';
 import H2Heading from './H2Heading';
 import AccomplishmentItem from './AccomplishmentItem';
 import AccomplishmentForm from '../components/AccomplishmentForm';
 
+const StyledWrapper = styled.div`
+  margin: 16px 0;
+`;
+
 const DiaryAccomplishment: React.FC = () => (
   <>
     <H2Heading text="今日達成したこと" color="#2cd671" />
-    <div css="margin: 16px 0;">
+    <StyledWrapper>
       <AccomplishmentItem text="5km走れた" />
       <AccomplishmentItem text="15km走れた" />
       <AccomplishmentItem text="25km走れた" />
-    </div>
+    </StyledWrapper>
     <AccomplishmentForm />
   </>
 );
