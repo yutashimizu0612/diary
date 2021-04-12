d01900f86e10fa589e80a868ada7652388f485da
yutashimizu0612 <s08388yskh@gmail.com>
2021-04-12 21:15:11 +0900

    マイグレーションにpublishedカラム追加

--- server/migrations/20210409230359-create-accomplishment.js
+++ server/migrations/20210409230359-create-accomplishment.js
@@ -21,6 +21,10 @@ module.exports = {
         },
         allowNull: false,
       },
+      published: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
