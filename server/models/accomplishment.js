691ab80d824e95a24b3a4774dc61561ca6a8252d
yutashimizu0612 <s08388yskh@gmail.com>
2021-04-13 20:32:33 +0900

    belongsToのオプションを設定

--- server/models/accomplishment.js
+++ server/models/accomplishment.js
@@ -9,7 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Accomplishment.belongsTo(models.User);
+      Accomplishment.belongsTo(models.User, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE',
+      });
     }
   }
   Accomplishment.init(
