fdb5f8f9cf90423dbc8b6eca3b310a8507e76722
yutashimizu0612 <s08388yskh@gmail.com>
2021-04-20 20:32:02 +0900

    カスタムフック（Accomplishment）を作成

---
+++ client/src/hooks/use-accomplishments.tsx
@@ -0,0 +1,35 @@
+import { useState, useCallback, useContext } from 'react';
+import axios from 'axios';
+import accomplishmentContext from '../views/contexts/AccomplishmentContext';
+import { useAuth } from '../functions/auth/use-auth';
+
+export const useAccomplishment = () => {
+  return useContext(accomplishmentContext);
+};
+
+const useProvideAccomplishment = () => {
+  const auth = useAuth();
+  const token = auth.getAccessToken();
+  const [accomplishments, setAccomplishments] = useState([]);
+
+  const getAccomplishments = useCallback(() => {
+    console.log('getAccomplishments');
+    return axios({
+      method: 'GET',
+      url: `${process.env.REACT_APP_API_URL}/accomplishments`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((response) => {
+      console.log('getAccomplishments SUCCESS', response);
+      setAccomplishments(response.data);
+    });
+  }, []);
+
+  return {
+    accomplishments,
+    getAccomplishments,
+  };
+};
+
+export default useProvideAccomplishment;
