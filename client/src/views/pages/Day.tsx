2aa2a0bd8b99e818af664509411f9ce8c33376fd
yutashimizu0612 <s08388yskh@gmail.com>
2021-04-21 21:07:31 +0900

    Dayでは、AccomplishmentAPIとの通信のロジックを持たないようにする

--- client/src/views/pages/Day.tsx
+++ client/src/views/pages/Day.tsx
@@ -1,13 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components/macro';
 import Layout from '../layouts/Layout';
+import Accomplishment from '../containers/diary/Accomplishment';
 import Contribution from '../components/Contribution';
 import DiaryDate from '../components/DiaryDate';
-import DiaryAccomplishment from '../components/DiaryAccomplishment';
 import DiaryComment from '../components/DiaryComment';
 import DiaryStar from '../components/DiaryStar';
 import DiaryPagination from '../components/DiaryPagination';
-import { useAccomplishment } from '../../hooks/use-accomplishments';
 
 const StyledWrapper = styled.div`
   margin: 60px auto 0;
@@ -19,18 +18,13 @@ const StyledWrapper = styled.div`
 // コントリビューションの取得
 
 const Day: React.FC = () => {
-  // Accomplishmentsの取得
-  const { accomplishments, getAccomplishments } = useAccomplishment();
-  useEffect(() => {
-    getAccomplishments();
-  }, []);
   return (
     <Layout>
       <Contribution />
       <StyledWrapper>
         <DiaryDate />
         <div css="margin-top: 45px;">
-          <DiaryAccomplishment accomplishments={accomplishments} />
+          <Accomplishment />
         </div>
         <div css="margin-top: 60px;">
           <DiaryComment />
