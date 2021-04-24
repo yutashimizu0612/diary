45595f8036826d7f53a7401fef3b062651fbe710
yutashimizu0612 <s08388yskh@gmail.com>
2021-04-24 19:34:22 +0900

    ProvideAuthからAuthProviderに名称変更

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { StylesProvider } from '@material-ui/core/styles';
 import GlobalStyle from './theme/globalStyles';
 import type {} from 'styled-components/cssprop';
-import ProvideAuth from './views/providers/ProvideAuth';
+import AuthProvider from './views/providers/AuthProvider';
 import AccomplishmentProvider from './views/providers/AccomplishmentProvider';
 import Routes from './Routes';
 
 const App: React.FC = () => (
-  <ProvideAuth>
+  <AuthProvider>
     <AccomplishmentProvider>
       <StylesProvider injectFirst>
         <GlobalStyle />
         <Routes />
       </StylesProvider>
     </AccomplishmentProvider>
-  </ProvideAuth>
+  </AuthProvider>
 );
 
 export default App;
