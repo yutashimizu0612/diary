103c4a9caf6301aebc9b4aac987907769d828306
yutashimizu0612 <s08388yskh@gmail.com>
2021-04-25 13:15:21 +0900

    コメントフォームコンポーネント削除

--- client/src/views/components/DiaryComment.tsx
+++ client/src/views/components/DiaryComment.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import styled from 'styled-components/macro';
 import H2Heading from './H2Heading';
-import DiaryCommentForm from './DiaryCommentForm';
+
+const StyledForm = styled.textarea`
+  background: #fff;
+  border-radius: 8px;
+  border: none;
+  box-shadow: 0 3px 4px rgb(0, 0, 0, 0.16);
+  cursor: pointer;
+  color: #3e3e3e;
+  font-size: 18px;
+  outline: none;
+  padding: 20px;
+  height: 350px;
+  width: 100%;
+`;
+
+// コメントの取得
+// コメントの新規作成
+// コメントの更新
 
 const DiaryComment: React.FC = () => (
   <>
     <H2Heading text="コメント" color="#f8548c" />
     <div css="margin: 16px 0;">
-      <DiaryCommentForm />
+      <StyledForm />
     </div>
   </>
 );
