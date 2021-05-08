d753bb21d94729631ff28bc248958565e12f3666
yutashimizu0612 <s08388yskh@gmail.com>
2021-05-08 12:36:06 +0900

    クライアント側：差分無理やり

--- client/src/views/pages/ActivateAccount.tsx
+++ client/src/views/pages/ActivateAccount.tsx
@@ -1,90 +1,89 @@
-// ActivateAccount.tsx
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components/macro';
-import axios from 'axios';
-import { Color } from '@material-ui/lab/Alert';
-import { useParams } from 'react-router-dom';
-import Layout from '../layouts/Layout';
-import Toast from '../components/Toast';
-import Button from '../components/Button';
+  import React, { useState, useEffect } from 'react';
+  import styled from 'styled-components/macro';
+  import axios from 'axios';
+  import { Color } from '@material-ui/lab/Alert';
+  import { useParams } from 'react-router-dom';
+  import Layout from '../layouts/Layout';
+  import Toast from '../components/Toast';
+  import Button from '../components/Button';
 
-const StyledWrapper = styled.div`
-  margin: 130px auto 0;
-  width: 400px;
-`;
+  const StyledWrapper = styled.div`
+    margin: 130px auto 0;
+    width: 400px;
+  `;
 
-const StyledTitle = styled.h2`
-  font-size: 24px;
-  font-weight: normal;
-  margin-bottom: 80px;
-  text-align: center;
-`;
+  const StyledTitle = styled.h2`
+    font-size: 24px;
+    font-weight: normal;
+    margin-bottom: 80px;
+    text-align: center;
+  `;
 
-type ParamTypes = {
-  token: string;
-};
+  type ParamTypes = {
+    token: string;
+  };
 
-const ActivateAccount: React.FC = () => {
-  const [values, setValues] = useState({ token: '' });
-  const { token } = useParams<ParamTypes>();
+  const ActivateAccount: React.FC = () => {
+    const [values, setValues] = useState({ token: '' });
+    const { token } = useParams<ParamTypes>();
 
-  useEffect(() => {
-    console.log('token', token);
-    if (token) {
-      setValues({ ...values, token });
-    }
-  }, []);
+    useEffect(() => {
+      console.log('token', token);
+      if (token) {
+        setValues({ ...values, token });
+      }
+    }, []);
 
-  const [toastStatus, setToastStatus] = useState({
-    isOpen: false,
-    message: '',
-    severity: 'success' as Color,
-  });
+    const [toastStatus, setToastStatus] = useState({
+      isOpen: false,
+      message: '',
+      severity: 'success' as Color,
+    });
 
-  const submit = (): void => {
-    console.log('submit');
-    const { token } = values;
-    axios({
-      method: 'POST',
-      url: `${process.env.REACT_APP_API_URL}/activation`,
-      data: { token },
-    })
-      .then((response) => {
-        console.log('ACCOUNT ACTIVATION SUCCESS', response);
-        setToastStatus({
-          isOpen: true,
-          message: response.data.message,
-          severity: 'success',
-        });
+    const submit = (): void => {
+      console.log('submit');
+      const { token } = values;
+      axios({
+        method: 'POST',
+        url: `${process.env.REACT_APP_API_URL}/activation`,
+        data: { token },
       })
-      .catch((error) => {
-        console.log('ACCOUNT ACTIVATION ERROR', error.response);
-        setToastStatus({
-          isOpen: true,
-          message: error.response.data.message,
-          severity: 'error',
+        .then((response) => {
+          console.log('ACCOUNT ACTIVATION SUCCESS', response);
+          setToastStatus({
+            isOpen: true,
+            message: response.data.message,
+            severity: 'success',
+          });
+        })
+        .catch((error) => {
+          console.log('ACCOUNT ACTIVATION ERROR', error.response);
+          setToastStatus({
+            isOpen: true,
+            message: error.response.data.message,
+            severity: 'error',
+          });
         });
-      });
+    };
+
+    const closeSnackBar = (): void => {
+      setToastStatus({ ...toastStatus, isOpen: false });
+    };
+
+    return (
+      <Layout>
+        <Toast
+          open={toastStatus.isOpen}
+          onClose={closeSnackBar}
+          message={toastStatus.message}
+          severity={toastStatus.severity}
+        />
+        <StyledWrapper>
+          <StyledTitle>アカウントの本登録</StyledTitle>
+          <Button text="登録する" appearance="primary" onClick={() => submit()} />
+        </StyledWrapper>
+      </Layout>
+    );
   };
 
-  const closeSnackBar = (): void => {
-    setToastStatus({ ...toastStatus, isOpen: false });
-  };
-
-  return (
-    <Layout>
-      <Toast
-        open={toastStatus.isOpen}
-        onClose={closeSnackBar}
-        message={toastStatus.message}
-        severity={toastStatus.severity}
-      />
-      <StyledWrapper>
-        <StyledTitle>アカウントの本登録</StyledTitle>
-        <Button text="登録する" appearance="primary" onClick={() => submit()} />
-      </StyledWrapper>
-    </Layout>
-  );
-};
-
-export default ActivateAccount;
+  export default ActivateAccount;
--- client/src/views/providers/AuthProvider.tsx
+++ client/src/views/providers/AuthProvider.tsx
@@ -1,11 +1,10 @@
-// AuthProvider.tsx
-import React from 'react';
-import authContext from '../contexts/AuthContext';
-import useProvideAuth from '../../functions/auth/use-auth';
+  import React from 'react';
+  import authContext from '../contexts/AuthContext';
+  import useProvideAuth from '../../functions/auth/use-auth';
 
-const ProvideAuth: React.FC = ({ children }) => {
-  const auth = useProvideAuth();
-  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
-};
+  const ProvideAuth: React.FC = ({ children }) => {
+    const auth = useProvideAuth();
+    return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+  };
 
-export default ProvideAuth;
+  export default ProvideAuth;
