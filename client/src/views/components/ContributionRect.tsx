db2045274a97cc72d166c5040e90095f4815be67
yutashimizu0612 <s08388yskh@gmail.com>
2021-05-13 09:40:13 +0900

    マウスホバー時にツールチップを表示する

--- client/src/views/components/ContributionRect.tsx
+++ client/src/views/components/ContributionRect.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components/macro';
 import { Count } from '../../types';
 import { lighten } from '@material-ui/core';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const StyledRect = styled.rect<{ level: number }>`
   fill: ${(props) => (props.level === 0 ? '#e8e2e2' : lighten('#3ea171', 1 - props.level * 0.2))};
+`;
+
+const StyledAccomplishment = styled.span`
+  display: block;
+  text-align: center;
+`;
+
+const StyledCount = styled.span`
+  display: inline-block;
+  font-size: 14px;
+  font-weight: bold;
+  margin: 0 0 0 6px;
+`;
+
+const StyledDate = styled.span`
+  color: #c6c6c6;
+  display: block;
+  font-size: 12px;
+  text-align: center;
 `;
 
 type Props = {
@@ -13,15 +33,42 @@ type Props = {
 };
 
 const ContributionRect: React.FC<Props> = ({ day, index }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  const title = (
+    <>
+      {day.count !== 0 && (
+        <StyledAccomplishment>
+          <StyledCount>{day.count}</StyledCount>つ達成しました！
+        </StyledAccomplishment>
+      )}
+      <StyledDate>{day.date}</StyledDate>
+    </>
+  );
   return (
-    <StyledRect
-      width="16"
-      height="16"
-      x="0"
-      y={index * 18}
-      level={day.count > 10 ? 5 : Math.ceil(day.count / 2)}
-      data-date={day.date}
-      data-count={day.count}></StyledRect>
+    <>
+      <Tooltip
+        open={isOpen}
+        title={title}
+        arrow
+        placement="top"
+        onClose={handleClose}
+        onOpen={handleOpen}>
+        <StyledRect
+          width="16"
+          height="16"
+          x="0"
+          y={index * 18}
+          level={day.count > 10 ? 5 : Math.ceil(day.count / 2)}
+          data-date={day.date}
+          data-count={day.count}></StyledRect>
+      </Tooltip>
+    </>
   );
 };
 
