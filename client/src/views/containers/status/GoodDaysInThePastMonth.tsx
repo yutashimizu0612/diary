409cf6296ff7926677d386e55484bb1c530d3632
yutashimizu0612 <s08388yskh@gmail.com>
2021-05-26 09:19:46 +0900

    productiveDayの型を使用 / コメント追記

--- client/src/views/containers/status/GoodDaysInThePastMonth.tsx
+++ client/src/views/containers/status/GoodDaysInThePastMonth.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/macro';
 import moment from 'moment';
-import { Accomplishment } from '../../../types';
+import { Accomplishment, productiveDay } from '../../../types';
 import H2Heading from '../../components/H2Heading';
 import GoodDays from '../../components/GoodDays';
 import { useAccomplishment } from '../../../hooks/use-accomplishments';
@@ -14,33 +14,31 @@ const GoodDaysInThePastMonth: React.FC = () => {
   const today = moment();
   const startDay = today.clone().subtract(1, 'months');
   const { getAccomplishments, getProductiveDays } = useAccomplishment();
-  const [productiveDays, setProductiveDays] = useState([]);
+  const [productiveDays, setProductiveDays] = useState<productiveDay[]>([]);
   useEffect(() => {
     (async () => {
-      // 達成したことの数が多い日を6つ取得
-      const data = await getProductiveDays(
+      // 1. 達成したことの数が多い日を6つ取得
+      const data: { date: string; count: number }[] = await getProductiveDays(
         startDay.format('YYYY-MM-DD'),
         today.format('YYYY-MM-DD'),
         'DESC',
         6,
       );
-      // 日付のみを含む配列に変換：['2021-05-26', '2021-04-22'....]
+      // 2. 取得したdataを日付のみを含む配列に変換：['2021-05-26', '2021-04-22'....]
       const dates = data.map((element: { date: string; count: number }) => element.date);
-      // 該当日の達成タスクを取得
-      const accomplishments = await getAccomplishments(dates);
-      // datesにcontentsプロパティを追加
-      const productiveDays = data.map(
-        (date: { date: string; count: number; contents: string[] }) => {
-          const contents: string[] = [];
-          accomplishments.forEach((accomplishment: Accomplishment) => {
-            if (date.date === accomplishment.date) {
-              contents.push(accomplishment.content);
-            }
-          });
-          date['contents'] = contents;
-          return date;
-        },
-      );
+      // 3. 2を元に該当日の達成タスクを取得
+      const accomplishments: Accomplishment[] = await getAccomplishments(dates);
+      // 4. 元dataにcontentsプロパティを追加（日付が一致する達成タスクをcontents配列に格納）
+      const productiveDays: productiveDay[] = data.map((date: productiveDay) => {
+        const contents: string[] = [];
+        accomplishments.forEach((accomplishment: Accomplishment) => {
+          if (date.date === accomplishment.date) {
+            contents.push(accomplishment.content);
+          }
+        });
+        date['contents'] = contents;
+        return date;
+      });
       setProductiveDays(productiveDays);
     })();
   }, []);
